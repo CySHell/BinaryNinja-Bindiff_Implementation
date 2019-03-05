ec2730d0dec189b35a14741a856ff222c525b7a8
CySHeLL <rowr12000@gmail.com>
2019-03-05 17:38:04 +0200

    start work on tracelets matcher V0.1

--- Factory/tracelets/Tracelets.py
+++ Factory/tracelets/Tracelets.py
@@ -1,54 +1,44 @@
+import itertools
+
 
 class TraceletsFunc:
     """
     This class contains the aggregate information of all tracelets in the given MLIL function
     """
 
-    def __init__(self, mlil_function):
+    def __init__(self, mlil_function, tracelet_block_limit):
         """
         :param mlil_function: (MLIL_FUNCTION) the function to extract tracelets from
         """
         self.func = mlil_function
-        self.tracelet_list = []         # a list of Tracelet objects extracted from the function
-        self.tracelet_count = 0         # amount of tracelets in the function
+        self.tracelet_list = []  # a list of Tracelet objects extracted from the function
+        self.tracelet_count = 0  # amount of tracelets in the function
+        self.tracelet_block_limit = tracelet_block_limit  # denotes how many MLIL basic_blocks compose a single tracelet
 
-    def extract_tracelets(self, tracelet_blocks):
+    def extract_tracelets(self):
         """
         extract all tracelets of length tracelet_length from the function
-        :param tracelet_blocks: (INT) denotes how many MLIL basic_blocks compose a single tracelet
         :return: tracelet_count: (INT) amount of tracelets in the function
         """
-        bb_in_func = self.func.basicblocks
+        bb_in_func = self.func.basic_blocks
         length_bb_in_func = len(bb_in_func)
 
-        if length_bb_in_func < tracelet_blocks:
+        if length_bb_in_func < self.tracelet_block_limit:
             # functions too small to extract tracelets
             self.tracelet_count = 0
             return 0
 
         for bb in bb_in_func:
-            self.tracelet_list.append(self.build_tracelet(bb))
+            self.tracelet_list.extend(self._extract(bb))
 
-    def build_tracelet(self,bb):
-
-class Tracelet:
-    """
-    This Class represents a single Tracelet
-    """
-
-    def __init__(self, bb, tracelet_block_count):
-        """
-        :param bb: (MLIL_BASIC_BLOCK) basic block starting the tracelet
-        :param tracelet_block_count: (INT) how many blocks in the tracelet
-        """
-        self.tracelet_block_count = tracelet_block_count
-        self.tracelet_blocks = [bb]
-        self._build_trace()
+    def _extract(self, bb):
+        curr_tracelet_len = self.tracelet_block_limit
+        print(bb)
+        if curr_tracelet_len == 1 or bb.outgoing_edges is None:
+            return [bb]
+        else:
+            for out_edge in bb.outgoing_edges:
+                return itertools.product([bb], self._extract(out_edge.target))
 
 
-    def _build_tracelet(self):
-        sucess = self.tracelet_block_count
-        for index in range(self.tracelet_block_count):
-            for outgoing_edges in self.tracelet_blocks[-1]
-            self.tracelet_block_count -= 1
-
+print(TraceletsFunc(current_mlil, 3).extract_tracelets())
