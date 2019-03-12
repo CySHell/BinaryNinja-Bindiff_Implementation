e39c88cad39aa602b5edadb9a13a93f869349fc5
CySHeLL <rowr12000@gmail.com>
2019-03-12 15:43:12 +0200

    tracelets matcher V0.4 instructions are encoded and stored properly.

--- __init__.py
+++ __init__.py
@@ -1,4 +1,6 @@
 from binaryninja import *
+import pprint
+
 #from .Matchers.Tracelet_Matcher import Data_Types_and_Constants, Normalization_Techniques, Tracelet_Matching
 from .Matchers.Tracelet_Matcher.Tracelet_Matching import *
 from .Matchers.Tracelet_Matcher.Normalization_Techniques import *
@@ -6,10 +8,18 @@ from .Matchers.Tracelet_Matcher.Data_Types_and_Constants import *
 
 
 def main(bv):
+    function_list = []
     for f in bv:
-        trace_list = extract_tracelets(f.medium_level_il, 3)
-        for TL in trace_list:
-            print(normalize_instructions(TL))
+        function_metadata = Data_Types_and_Constants.TracedFunction()
+        function_metadata.filename = bv.file.filename
+        function_metadata.raw_file_offset = f.start - f.view.start
 
+        trace_list = extract_tracelets(f.medium_level_il, 3)
+
+        for TL in trace_list:
+            tracelet = normalize_tracelet(TL)
+            function_metadata.tracelets.append(tracelet)
+
+        function_metadata.pretty_print()
 
 PluginCommand.register("BNB - BinDja", "BNB - Bindiff Implementation for Binja", main)
